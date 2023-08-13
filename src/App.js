1149fa28f0c4596190facc0f3456a149e01bd7ce
Krizhinka <127995890+Krizhinka@users.noreply.github.com>
2023-08-14 01:09:53 +0300

    Added GitHub repo url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Create Weather App</div>;
+  return (
+    <div className="App">
+      React Weather App
+      <footer>
+        This project was coded by Olga Kryzhanovska and is{" "}
+        <a
+          href="https://github.com/Krizhinka/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
