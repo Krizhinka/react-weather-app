ca836f18edd430f16077f04c3dc8d04185a61969
Krizhinka <127995890+Krizhinka@users.noreply.github.com>
2023-08-31 01:32:27 +0300

    Update WeatherIcon.js

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -4,7 +4,7 @@ import ReactAnimatedWeather from "react-animated-weather";
 export default function WeatherIcon(props) {
   console.log(props.code);
   const codeMapping = {
-    "clear - sky - day": "CLEAR_DAY",
+    "clear-sky-day": "CLEAR_DAY",
     "clear-sky-night": "CLEAR_NIGHT",
     "few-clouds-day": "PARTLY_CLOUDY_DAY",
     "few-clouds-night": "PARTLY_CLOUDY_NIGHT",
